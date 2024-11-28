ac723c47e54e5e2033ce62f339778e3e8ca050e0
juules32 <56916699+Juules32@users.noreply.github.com>
2024-11-28 16:43:34 +0100

    added board state and piece types and occupancies

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,6 +1,5 @@
 use core::fmt;
 use std::ops::*;
-
 use crate::square::Square;
 
 #[derive(Clone, Copy)]
@@ -31,54 +30,52 @@ impl Bitboard {
     pub fn is_not_empty(&self) -> bool {
         self.0 != 0
     }
-
-    pub fn print(&self) {
-        println!("{}", &self);
-    }
 }
 
 #[allow(dead_code)]
 impl Bitboard {
-    pub const FILE_A: Bitboard = Bitboard(0xff);
-    pub const FILE_B: Bitboard = Bitboard(0xff00);
-    pub const FILE_C: Bitboard = Bitboard(0xff0000);
-    pub const FILE_D: Bitboard = Bitboard(0xff000000);
-    pub const FILE_E: Bitboard = Bitboard(0xff00000000);
-    pub const FILE_F: Bitboard = Bitboard(0xff0000000000);
-    pub const FILE_G: Bitboard = Bitboard(0xff000000000000);
-    pub const FILE_H: Bitboard = Bitboard(0xff00000000000000);
+    pub const FILE_A: Bitboard = Bitboard(0x101010101010101);
+    pub const FILE_B: Bitboard = Bitboard(0x202020202020202);
+    pub const FILE_C: Bitboard = Bitboard(0x404040404040404);
+    pub const FILE_D: Bitboard = Bitboard(0x808080808080808);
+    pub const FILE_E: Bitboard = Bitboard(0x1010101010101010);
+    pub const FILE_F: Bitboard = Bitboard(0x2020202020202020);
+    pub const FILE_G: Bitboard = Bitboard(0x4040404040404040);
+    pub const FILE_H: Bitboard = Bitboard(0x8080808080808080);
 
-    pub const RANK_1: Bitboard = Bitboard(0x101010101010101);
-    pub const RANK_2: Bitboard = Bitboard(0x202020202020202);
-    pub const RANK_3: Bitboard = Bitboard(0x404040404040404);
-    pub const RANK_4: Bitboard = Bitboard(0x808080808080808);
-    pub const RANK_5: Bitboard = Bitboard(0x1010101010101010);
-    pub const RANK_6: Bitboard = Bitboard(0x2020202020202020);
-    pub const RANK_7: Bitboard = Bitboard(0x4040404040404040);
-    pub const RANK_8: Bitboard = Bitboard(0x8080808080808080);
+    pub const RANK_8: Bitboard = Bitboard(0xFF);
+    pub const RANK_7: Bitboard = Bitboard(0xFF00);
+    pub const RANK_6: Bitboard = Bitboard(0xFF0000);
+    pub const RANK_5: Bitboard = Bitboard(0xFF000000);
+    pub const RANK_4: Bitboard = Bitboard(0xFF00000000);
+    pub const RANK_3: Bitboard = Bitboard(0xFF0000000000);
+    pub const RANK_2: Bitboard = Bitboard(0xFF000000000000);
+    pub const RANK_1: Bitboard = Bitboard(0xFF00000000000000);
 
-    pub const WHITE_SQUARES: Bitboard = Bitboard(0x55AA55AA55AA55AA);
-    pub const BLACK_SQUARES: Bitboard = Bitboard(0xAA55AA55AA55AA55);
+    pub const WHITE_SQUARES: Bitboard = Bitboard(0xAA55AA55AA55AA55);
+    pub const BLACK_SQUARES: Bitboard = Bitboard(0x55AA55AA55AA55AA);
+
+    pub const WHITE_PIECES: Bitboard = Bitboard(0xFFFF000000000000);
+    pub const BLACK_PIECES: Bitboard = Bitboard(0xFFFF);
+    pub const ALL_PIECES: Bitboard = Bitboard(0xFFFF00000000FFFF);
+    pub const EMPTY: Bitboard = Bitboard(0x0);
+
+    pub const BP: Bitboard = Bitboard::RANK_7;
+    pub const BN: Bitboard = Bitboard(0x42);
+    pub const BB: Bitboard = Bitboard(0x24);
+    pub const BR: Bitboard = Bitboard(0x81);
+    pub const BQ: Bitboard = Bitboard(0x8);
+    pub const BK: Bitboard = Bitboard(0x10);
+
+    pub const WP: Bitboard = Bitboard::RANK_2;
+    pub const WN: Bitboard = Bitboard(0x4200000000000000);
+    pub const WB: Bitboard = Bitboard(0x2400000000000000);
+    pub const WR: Bitboard = Bitboard(0x8100000000000000);
+    pub const WQ: Bitboard = Bitboard(0x800000000000000);
+    pub const WK: Bitboard = Bitboard(0x1000000000000000);
 }
 
-impl fmt::Display for Bitboard {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut s = String::new();
-        for i in 0..8 {
-            s += &format!("  {}  ", 8 - i);
-            for j in 0..8 {
-                let square = (self.0 >> (i * 8 + j)) & 1;
-                s += if square != 0 {"O "} else {". "};
-            }
-            s += "\n";
-        }
-        s += "\n    a b c d e f g h\n";
-        s += &format!("\nBitboard: {}\n", self.0);
-        f.pad(&s)
-    }
-}
-
-macro_rules! impl_binary_ops_for_types {
+macro_rules! impl_bb_op_for_type {
     ($trait:ident, $method:ident, $op:tt, $rhs:ty) => {
         impl $trait<$rhs> for Bitboard {
             type Output = Bitboard;
@@ -91,10 +88,27 @@ macro_rules! impl_binary_ops_for_types {
     };
 }
 
-impl_binary_ops_for_types!(Shl, shl, <<, Square);
-impl_binary_ops_for_types!(BitAnd, bitand, &, Bitboard);
-impl_binary_ops_for_types!(BitAnd, bitand, &, Square);
-impl_binary_ops_for_types!(BitOr, bitor, |, Bitboard);
-impl_binary_ops_for_types!(BitOr, bitor, |, Square);
-impl_binary_ops_for_types!(BitXor, bitxor, ^, Bitboard);
-impl_binary_ops_for_types!(BitXor, bitxor, ^, Square);
+impl_bb_op_for_type!(Shl, shl, <<, Square);
+impl_bb_op_for_type!(BitAnd, bitand, &, Bitboard);
+impl_bb_op_for_type!(BitAnd, bitand, &, Square);
+impl_bb_op_for_type!(BitOr, bitor, |, Bitboard);
+impl_bb_op_for_type!(BitOr, bitor, |, Square);
+impl_bb_op_for_type!(BitXor, bitxor, ^, Bitboard);
+impl_bb_op_for_type!(BitXor, bitxor, ^, Square);
+
+impl fmt::Display for Bitboard {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut s = String::new();
+        for i in 0..8 {
+            s += &format!("  {}  ", 8 - i);
+            for j in 0..8 {
+                let square = (self.0 >> (i * 8 + j)) & 1;
+                s += if square != 0 {"O "} else {". "};
+            }
+            s += "\n";
+        }
+        s += "\n     a b c d e f g h\n";
+        s += &format!("\nBitboard: {}\n", self.0);
+        f.pad(&s)
+    }
+}
---
+++ src/board_state.rs
@@ -0,0 +1,101 @@
+use core::fmt;
+
+use crate::{bitboard::Bitboard, piece::PieceType, square::Square};
+
+#[derive(Clone)]
+pub struct BoardState {
+    pub bbs: [Bitboard; 12],
+    pub wo: Bitboard,
+    pub bo: Bitboard,
+    pub ao: Bitboard,
+}
+
+impl BoardState {
+
+    #[inline(always)]
+    pub fn merge_occupancies(&mut self) {
+        self.ao = self.wo | self.bo;
+    }
+
+    #[inline(always)]
+    pub fn populate_occupancies(&mut self) {
+        self.wo = 
+            self.bbs[PieceType::WP] | 
+            self.bbs[PieceType::WN] |
+            self.bbs[PieceType::WB] |
+            self.bbs[PieceType::WR] |
+            self.bbs[PieceType::WQ] |
+            self.bbs[PieceType::WK];
+        self.bo = 
+            self.bbs[PieceType::BP] | 
+            self.bbs[PieceType::BN] |
+            self.bbs[PieceType::BB] |
+            self.bbs[PieceType::BR] |
+            self.bbs[PieceType::BQ] |
+            self.bbs[PieceType::BK];
+        
+        self.merge_occupancies();
+    }
+
+    pub fn starting_position() -> BoardState {
+        BoardState {
+            bbs: [
+                Bitboard::WP,
+                Bitboard::WN,
+                Bitboard::WB,
+                Bitboard::WR,
+                Bitboard::WQ,
+                Bitboard::WK,
+                Bitboard::BP,
+                Bitboard::BN,
+                Bitboard::BB,
+                Bitboard::BR,
+                Bitboard::BQ,
+                Bitboard::BK,
+            ],
+            wo: Bitboard::WHITE_PIECES,
+            bo: Bitboard::BLACK_PIECES,
+            ao: Bitboard::ALL_PIECES
+        }
+    }
+
+    pub fn set_piece(&mut self, piece: PieceType, sq: Square) {
+        self.bbs[piece].set_sq(sq);
+    }
+}
+
+impl Default for BoardState {
+    fn default() -> BoardState {
+        BoardState {
+            bbs: [Bitboard::EMPTY; 12],
+            wo: Bitboard::EMPTY,
+            bo: Bitboard::EMPTY,
+            ao: Bitboard::EMPTY
+        }
+    }
+}
+
+impl fmt::Display for BoardState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut s = String::new();
+        for rank in 0..8 {
+            s += &format!("  {}  ", 8 - rank);
+            for file in 0..8 {
+                let mut is_occupied = false;
+                let sq = Square(rank * 8 + file);
+                for piece_type in PieceType::ALL {
+                    if Bitboard::is_set_sq(&self.bbs[piece_type], sq) {
+                        s += &format!("{} ", piece_type.to_string());
+                        is_occupied = true;
+                    }
+                }
+                if !is_occupied {
+                    s += ". ";
+                }
+            }
+            s += "\n";
+        }
+        s += "\n     a b c d e f g h\n";
+        f.pad(&s)
+    }
+}
