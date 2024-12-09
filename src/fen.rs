f5f9db2c9ac20e3494ece2cbbb8780aa91821e6b
juules32 <56916699+Juules32@users.noreply.github.com>
2024-12-09 19:27:44 +0100

    better error handling for fen parser

--- src/fen.rs
+++ src/fen.rs
@@ -6,33 +6,40 @@ pub const ROOK_POSITION: &str = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -";
 pub const TRICKY_POSITION: &str = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ -";
 pub const TRICKY_POSITION_2: &str = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
 
-fn set_pieces(board_state: &mut BoardState, pieces_str: &str) {
+#[derive(Debug)]
+pub struct FenParseError(&'static str);
+
+fn set_pieces(board_state: &mut BoardState, pieces_str: &str) -> Result<(), FenParseError> {
     let mut sq_index = 0_u8;
     for pieces_char in pieces_str.chars() {
         match pieces_char {
-            '1'..='8' => sq_index += pieces_char.to_digit(10).expect("Could not convert pieces_char to digit!") as u8,
+            '1'..='8' => sq_index += pieces_char
+                .to_digit(10)
+                .ok_or(FenParseError("Could not convert char to digit!"))? as u8,
             '/' => (),
-            _ => {
+            'P' | 'N' | 'B' | 'R' | 'Q' | 'K' | 'p' | 'n' | 'b' | 'r' | 'q' | 'k' => {
                 let piece_type = PieceType::from(pieces_char);
                 board_state.set_piece(piece_type, Square::from(sq_index));
             }
+            _ => return Err(FenParseError("Invalid pieces!"))
         };
         if !pieces_char.is_digit(10) && pieces_char != '/' { sq_index += 1; }
     }
     board_state.populate_occupancies();
+    Ok(())
 }
 
-fn set_side(board_state: &mut BoardState, side_str: &str) {
-    if side_str == "w" {
-        board_state.side = Color::White;
-    } else if side_str == "b" {
-        board_state.side = Color::Black;
-    } else {
-        panic!("Incorrect side_str!");
+fn set_side(board_state: &mut BoardState, side_str: &str) -> Result<(), FenParseError> {
+    match side_str {
+        "w" => board_state.side = Color::White,
+        "b" => board_state.side = Color::Black,
+        _ => return Err(FenParseError("Invalid side!")),
     }
+
+    Ok(())
 }
 
-fn set_castling_rights(board_state: &mut BoardState, castling_rights_str: &str) {
+fn set_castling_rights(board_state: &mut BoardState, castling_rights_str: &str) -> Result<(), FenParseError> {
     for char in castling_rights_str.chars() {
         match char {
             'K' => board_state.castling_rights.0 |= CastlingRights::WK.0,
@@ -40,30 +47,37 @@ fn set_castling_rights(board_state: &mut BoardState, castling_rights_str: &str) 
             'k' => board_state.castling_rights.0 |= CastlingRights::BK.0,
             'q' => board_state.castling_rights.0 |= CastlingRights::BQ.0,
             '-' => (),
-            _ => panic!("Illegal char in castling_rights_str found!")
+            _ => return Err(FenParseError("Invalid castling rights!")),
+        }
+    }
+
+    Ok(())
+}
+
+fn set_en_passant_sq(board_state: &mut BoardState, en_passant_sq_str: &str) -> Result<(), FenParseError> {
+    match en_passant_sq_str {
+        "-" => Ok(()),
+        _ => {
+            board_state.en_passant_sq = Square::try_from(en_passant_sq_str)
+                .map_err(|_| FenParseError("Invalid en-passant square!"))?;
+            Ok(())
         }
     }
 }
 
-fn set_en_passant_sq(board_state: &mut BoardState, en_passant_sq_str: &str) {
-    if en_passant_sq_str != "-" {
-        board_state.en_passant_sq = Square::from(en_passant_sq_str);
-    }
-}
-
-pub fn parse(fen_string: &str) -> BoardState {
+pub fn parse(fen_string: &str) -> Result<BoardState, FenParseError> {
     let mut bs = BoardState::default();
 
     let mut fen_iter = fen_string.split_whitespace();
-    let pieces_str = fen_iter.next().expect("No pieces_str found!");
-    let side_str = fen_iter.next().expect("No side_str found!");
-    let castling_rights_str = fen_iter.next().expect("No castling_rights_str found!");
-    let en_passant_sq_str = fen_iter.next().expect("No en_passant_sq_str found!");
+    let pieces_str = fen_iter.next().ok_or(FenParseError("No pieces found!"))?;
+    let side_str = fen_iter.next().ok_or(FenParseError("No side found!"))?;
+    let castling_rights_str = fen_iter.next().ok_or(FenParseError("No castling rights found!"))?;
+    let en_passant_sq_str = fen_iter.next().ok_or(FenParseError("No en-passant found!"))?;
 
-    set_pieces(&mut bs, pieces_str);
-    set_side(&mut bs, side_str);
-    set_castling_rights(&mut bs, castling_rights_str);
-    set_en_passant_sq(&mut bs, en_passant_sq_str);
+    set_pieces(&mut bs, pieces_str)?;
+    set_side(&mut bs, side_str)?;
+    set_castling_rights(&mut bs, castling_rights_str)?;
+    set_en_passant_sq(&mut bs, en_passant_sq_str)?;
 
-    bs
+    Ok(bs)
 }
