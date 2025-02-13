7f32a647874509d92c83bcbf91ab1a949dbc1d66
Benjamin Jensen <Benjaminbyvej@gmail.com>
2025-02-13 18:23:56 +0100

    uci line reads runs in its own thread, enabling the "stop" command, which has also been implemented

--- src/search.rs
+++ src/search.rs
@@ -1,25 +1,33 @@
 extern crate rand;
 
 use rand::Rng;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 use crate::{bit_move::ScoringMove, eval::Eval, move_generation::MoveGeneration, pl, position::Position, timer::Timer};
 
 pub struct Search {
     timer: Timer,
     stop_time: u128,
-    stop_calculating: bool,
+    pub stop_calculating: Arc<AtomicBool>,
     nodes: u64,
-    // pv, killer_moves, etc...
+    // TODO: pv, killer_moves, etc...
+}
+
+impl Default for Search {
+    fn default() -> Search {
+        Search {
+            timer: Timer::new(),
+            stop_time: 0,
+            stop_calculating: Arc::new(AtomicBool::new(false)),
+            nodes: 0,
+        }
+    }
 }
 
 impl Search {
-    pub fn new(stop_time: u128) -> Search {
-        Search {
-            timer: Timer::new(),
-            stop_time,
-            stop_calculating: false,
-            nodes: 0,
-        }
+    pub fn stop(&self) {
+        self.stop_calculating.store(true, Ordering::Relaxed);
     }
 
     fn random_best_move(&self, position: &Position, _depth: u8) -> ScoringMove {
@@ -31,10 +39,10 @@ impl Search {
         self.nodes += 1;
 
         if self.nodes % 5000 == 0 && self.timer.get_time_passed_millis() > self.stop_time {
-            self.stop_calculating = true;
+            self.stop();
         }
 
-        if self.stop_calculating {
+        if self.stop_calculating.load(Ordering::Relaxed) {
             return ScoringMove::blank(12345)
         }
         
@@ -63,25 +71,35 @@ impl Search {
             })
     }
 
-    fn best_scoring_move(&mut self, position: &mut Position, depth: u8) -> ScoringMove {
+    fn best_move(&mut self, position: &mut Position, depth: u8) -> ScoringMove {
         #[cfg(feature = "search_random")]
         return self.random_best_move(position, depth);
         
         #[cfg(feature = "search_minimax")]
         return self.minimax_best_move(position, depth);
     }
-    
-    pub fn go(&mut self, position: &mut Position, depth: u8) {
-        //TODO: Implement conditional iterative deepening here
-        println!("Searching for best move within {} milliseconds", self.stop_time);
+
+    pub fn go(&mut self, position: &mut Position, depth: u8, total_time: u128, increment: u128) {
+        self.stop_time = Self::calculate_stop_time(total_time, increment);
+        self.nodes = 0;
+        self.timer.reset();
+        self.stop_calculating.store(false, Ordering::Relaxed);
+
+        println!("info string searching for best move within {} milliseconds", self.stop_time);
 
         #[cfg(feature = "iterative_deepening")]
         {
             let mut best_scoring_move = ScoringMove::blank(13243);
             for current_depth in 1..=depth {
+                if current_depth != 1 {
+                    if self.timer.get_time_passed_millis() * Self::AVERAGE_AMOUNT_OF_MOVES > self.stop_time {
+                        pl!("info string ended iterative search early based on time prediction");
+                        break
+                    }
+                }
                 self.nodes = 0;
-                let new_best_move = self.best_scoring_move(position, current_depth);
-                if self.stop_calculating {
+                let new_best_move = self.best_move(position, current_depth);
+                if self.stop_calculating.load(Ordering::Relaxed) {
                     break
                 }
                 best_scoring_move = new_best_move;
