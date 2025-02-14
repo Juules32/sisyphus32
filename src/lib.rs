2e6fd1f6060c25b7fb0ca7d08eae71169d40422c
Benjamin Jensen <Benjaminbyvej@gmail.com>
2025-02-14 18:59:37 +0100

    added lib.rs for flexibility and integration testing

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#![allow(dead_code)]
+
+pub mod bit_move;
+pub mod bitboard;
+pub mod position;
+pub mod castling_rights;
+pub mod color;
+pub mod uci;
+pub mod fen;
+pub mod file;
+pub mod macros;
+pub mod magic_bitboards;
+pub mod move_masks;
+pub mod move_list;
+pub mod piece;
+pub mod rank;
+pub mod square;
+pub mod timer;
+pub mod perft;
+pub mod bit_twiddles;
+pub mod move_flag;
+pub mod search;
+pub mod eval;
+pub mod move_generation;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 // NOTE: The following pairs of features are not allowed to be used together:
 #[cfg(all(feature = "perft_parallelize", feature = "perft_single_thread"))]
 compile_error!("feature \"perft_parallelize\" and feature \"perft_single_thread\" cannot be enabled at the same time!");
@@ -16,32 +14,7 @@ compile_error!("feature \"sliders_magic_bitboards\" and feature \"sliders_on_the
 #[cfg(all(feature = "revert_with_undo_move", feature = "board_representation_array"))]
 compile_error!("feature \"revert_with_undo_move\" and feature \"board_representation_array\" cannot be enabled at the same time!");
 
-mod bit_move;
-mod bitboard;
-mod position;
-mod castling_rights;
-mod color;
-mod uci;
-mod fen;
-mod file;
-mod macros;
-mod magic_bitboards;
-mod move_masks;
-mod move_list;
-mod piece;
-mod rank;
-mod square;
-mod timer;
-mod perft;
-mod bit_twiddles;
-mod move_flag;
-mod search;
-mod eval;
-mod move_generation;
-
-use uci::Uci;
-
 fn main() {
-    move_masks::init();
-    Uci::default().init();
+    sisyphus32::move_masks::init();
+    sisyphus32::uci::Uci::default().init();
 }
