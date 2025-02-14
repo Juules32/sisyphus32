d32c5b0d2c8f9dae483032926ab12701d0f54c68
Benjamin Jensen <Benjaminbyvej@gmail.com>
2025-02-14 23:07:44 +0100

    general cleanup and prettier move generation function 🧹

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
 #![allow(dead_code)]
 
+// NOTE: The following pairs of features are not allowed to be used together:
+#[cfg(all(feature = "perft_parallelize", feature = "perft_single_thread"))]
+compile_error!("feature \"perft_parallelize\" and feature \"perft_single_thread\" cannot be enabled at the same time!");
+
+#[cfg(all(feature = "board_representation_bitboard", feature = "board_representation_array"))]
+compile_error!("feature \"board_representation_bitboard\" and feature \"board_representation_array\" cannot be enabled at the same time!");
+
+#[cfg(all(feature = "revert_with_clone", feature = "revert_with_undo_move"))]
+compile_error!("feature \"revert_with_clone\" and feature \"revert_with_undo_move\" cannot be enabled at the same time!");
+
+#[cfg(all(feature = "sliders_magic_bitboards", feature = "sliders_on_the_fly"))]
+compile_error!("feature \"sliders_magic_bitboards\" and feature \"sliders_on_the_fly\" cannot be enabled at the same time!");
+
+#[cfg(all(feature = "revert_with_undo_move", feature = "board_representation_array"))]
+compile_error!("feature \"revert_with_undo_move\" and feature \"board_representation_array\" cannot be enabled at the same time!");
+
 pub mod bit_move;
 pub mod bitboard;
 pub mod position;
