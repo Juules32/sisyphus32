9901939acf385689a575fa4cb664418f4a1d48de
juules32 <56916699+Juules32@users.noreply.github.com>
2025-02-19 22:22:50 +0100

    cleanup

--- src/bin/samply_go.rs
+++ src/bin/samply_go.rs
@@ -1,8 +1,6 @@
-use std::u128;
-
 use sisyphus32::{fen::FenString, search::Search};
 
 fn main() {
     let mut search = Search::default();
-    search.go(&FenString::kiwipete().parse().unwrap(), 7, u128::MAX, 0);
+    search.go(&FenString::kiwipete().parse().unwrap(), 7, None);
 }
