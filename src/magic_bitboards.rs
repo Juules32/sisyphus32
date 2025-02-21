abd28855ca7ace6e0c69ed37ad1d3c9facfe9c1d
Benjamin Jensen <Benjaminbyvej@gmail.com>
2025-02-21 22:29:02 +0100

    manual rng and new feature

--- src/magic_bitboards.rs
+++ src/magic_bitboards.rs
@@ -1,30 +1,15 @@
-use crate::{bitboard::Bitboard, move_masks::{self, MoveMasks}, square::Square};
+use crate::{bitboard::Bitboard, move_masks::{self, MoveMasks}, rng::RandomNumberGenerator, square::Square};
 
 const MAX_SLIDER_MOVE_PERMUTATIONS: usize = 4096;
 
-pub struct MagicBitboardGenerator {
-    pub seed: u32
+#[derive(Default)]
+struct MagicBitboardGenerator {
+    rng: RandomNumberGenerator
 }
 
 impl MagicBitboardGenerator {
-    fn generate_u32(&mut self) -> u32 {
-        self.seed ^= self.seed << 13; 
-        self.seed ^= self.seed >> 17; 
-        self.seed ^= self.seed << 5;
-        self.seed
-    }
-
-    fn generate_u64(&mut self) -> u64 {
-        let [a, b, c, d] = std::array::from_fn(|_| (self.generate_u32() & 0xFFFF) as u64);
-        a | (b << 16) | (c << 32) | (d << 48)
-    }
-
-    fn generate_sparse_u64(&mut self) -> u64 {
-        self.generate_u64() & self.generate_u64() & self.generate_u64()
-    }
-
     fn generate_magic_bitboard_candidate(&mut self) -> Bitboard {
-        Bitboard(self.generate_sparse_u64())
+        Bitboard(self.rng.generate_sparse_u64())
     }
 
     pub fn generate_magic_bitboard(&mut self, square: Square, num_relevant_bits: u8, is_bishop: bool) -> Bitboard {
@@ -74,7 +59,7 @@ impl MagicBitboardGenerator {
         panic!("No magic bitboard could be found");
     }
 
-    // Outputs magic bitboards which can be copied and used for move generation
+    // Prints magic bitboards which can be copied and used for move generation
     pub fn print_magic_bitboards(&mut self) {
 
         println!("\nRook magic bitboards:");
