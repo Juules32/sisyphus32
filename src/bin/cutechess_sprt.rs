d64a79e49cf6d77907b070e0bbca9b2cb45248aa
Benjamin Jensen <Benjaminbyvej@gmail.com>
2025-02-26 14:43:13 +0100

    modified cutechess script

--- src/bin/cutechess_sprt.rs
+++ src/bin/cutechess_sprt.rs
@@ -6,9 +6,8 @@ use std::{env, process::{exit, Command}, time::{SystemTime, UNIX_EPOCH}};
 
 const OPENINGS_NAME: &str = "openings";
 const PROFILE_NAME: &str = "release-all";
-const PLIES: &str = "5";
 const OPENING_PLIES: &str = "6";
-const GAMES: &str = "10";
+const GAMES: &str = "1";
 const ROUNDS: &str = "1000";
 const REPEAT: &str = "2";
 const MAX_MOVES: &str = "200";
@@ -38,7 +37,7 @@ fn main() {
         .args([
             "-engine", &format!("name={new_version}"), &format!("cmd=./{new_version}.exe"),
             "-engine", &format!("name={old_version}"), &format!("cmd=./{old_version}.exe"),
-            "-each", "tc=inf/10+0.1", &format!("dir=./target/{PROFILE_NAME}/"), "proto=uci", &format!("depth={PLIES}"),
+            "-each", "tc=1+0.1", &format!("dir=./target/{PROFILE_NAME}/"), "proto=uci",
             "-openings", &format!("file=openings/{OPENINGS_NAME}.pgn"), "order=random", &format!("plies={OPENING_PLIES}"),
             "-games", GAMES, "-rounds", ROUNDS, "-repeat", REPEAT, "-maxmoves", MAX_MOVES, "-concurrency", CONCURRENCY, "-ratinginterval", RATING_INTERVAL,
             "-sprt", &format!("elo0={ELO0}"), &format!("elo1={ELO1}"), &format!("alpha={ALPHA}"), &format!("beta={BETA}"),
