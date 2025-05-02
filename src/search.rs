c860c8d6fc29e8040f68e2087c160e051906e0bb
juules32 <56916699+Juules32@users.noreply.github.com>
2025-05-02 20:02:25 +0200

    cleanup

--- src/search.rs
+++ src/search.rs
@@ -462,15 +462,16 @@ impl Search {
 
         #[cfg(feature = "unit_syzygy_tablebase")]
         if let Some(tablebase) = self.tablebase.as_ref() {
-            if position.all_occupancy.count_bits() <= tablebase.get_max_pieces() + 1 {
+            let tablebase_max_pieces_u8 = tablebase.get_max_pieces() as u8;
+            if position.all_occupancy.count_bits() <= tablebase_max_pieces_u8 + 1 {
                 println!("info string searching for tablebase move");
                 let mut best_move_option = None;
 
-                if position.all_occupancy.count_bits() <= tablebase.get_max_pieces() && stop_time.is_none_or(|time| time >= TABLEBASE_SEARCH_THRESHOLD) {
+                if position.all_occupancy.count_bits() <= tablebase_max_pieces_u8 && stop_time.is_none_or(|time| time >= TABLEBASE_SEARCH_THRESHOLD) {
                     if let Some(tablebase_move) = tablebase.best_move(position) {
                         best_move_option = Some(tablebase_move);
                     }
-                } else if position.all_occupancy.count_bits() == tablebase.get_max_pieces() + 1 && stop_time.is_none_or(|time| time >= EXTENDED_TABLEBASE_SEARCH_THRESHOLD) {
+                } else if position.all_occupancy.count_bits() == tablebase_max_pieces_u8 + 1 && stop_time.is_none_or(|time| time >= EXTENDED_TABLEBASE_SEARCH_THRESHOLD) {
                     let mut moves = MoveGeneration::generate_captures::<ScoringMove, PseudoLegal>(position);
                     for scoring_move in moves.iter_mut() {
                         let mut new_position = position.clone();
