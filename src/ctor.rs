a631ca13ceae7bdf3913a540efa7c9bb3b53070c
juules32 <56916699+Juules32@users.noreply.github.com>
2025-05-24 21:08:13 +0200

    test

--- src/ctor.rs
+++ src/ctor.rs
@@ -2,7 +2,12 @@ use ctor::ctor;
 
 use crate::{eval_position::EvalPosition, move_masks::MoveMasks, transposition_table::TranspositionTable, zobrist::ZobristKey};
 
+#[cfg(not(target_arch = "wasm32"))]
 #[ctor]
+unsafe fn ctor() {
+    init();
+}
+
 unsafe fn init() {
     MoveMasks::init_move_masks();
     EvalPosition::init_positional_masks();
